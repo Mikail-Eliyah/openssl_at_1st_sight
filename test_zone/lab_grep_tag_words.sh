3a5cb21426fde729c2f2d526459d3db0d896800c
Mikail-Eliyah <Mikail.Eliyah@gmail.com>
2020-12-27 18:03:03 -0800

    updated test_zone/lab_grep_tag_words.sh

--- test_zone/lab_grep_tag_words.sh
+++ test_zone/lab_grep_tag_words.sh
@@ -1,4 +1,4 @@
-source $HOME"/include.sh"
+source $HOME"/scripts/include.sh"
 
 # main
 # grep tag words
@@ -41,6 +41,8 @@ echo $demarcator
 path_start='./test_data'
 grep_fields_with_different_start_and_end_labels;
 
+label="[case 02]"
+print_label;
 # 
 label_start='<meta-function>'
 label_end='<\/meta-function>'
@@ -53,6 +55,41 @@ path_start='./test_data'
 # grep_fields_with_different_start_and_end_labels;
 
 find $path_start -regex ".*\.\($file_type\)" -type f -exec awk '/'$label_start'/,/'$label_end'/ {print FILENAME ":" FNR ":" $0}' {} \; | sed -e 's/'$label_inner_start'/ /g' | sed -e 's/'$label_inner_end'/ /g'
+
+statement="ok  test front <meta-function> 101 </meta-function> test end "
+
+label="[case 03]"
+print_label;
+grep_fields_with_different_start_and_end_labels_in_line_retaining_labels "$statement"
+echo ""
+echo $demarcator
+echo $demarcator
+# extract text between tag and remove tags
+echo "$statement" | grep -noP '(?<=<meta-function>).*(?=</meta-function)' 
+
+label="[grep_fields_with_different_start_and_end_labels_in_line_without_labels]"
+print_label;
+grep_fields_with_different_start_and_end_labels_in_line_without_labels "$statement"
+echo ""
+echo $demarcator
+echo $demarcator
+echo "$statement" | grep -oP '(?<=<meta-function>).*(?=</meta-function)' 
+
+echo "$statement" | sed -n '/<meta-function>/,/<\/meta-function>/p'
+
+# get comment blocks
+# sed -n '/<start>/,/<\/start>/p' scores.txt
+
+grep_fields_with_different_start_and_end_labels_in_line_retaining_labels "test  202  test "
+
+# get comment blocks with line numbers
+#sed -n '/<start>/,/<\/start>/{p
+#=
+#d
+#}' scores.txt
+
+# get comment blocks with line numbers
+#cat -n scores.txt | sed -n "/<start>/,/<\/start>/p"
 
 ### Notes 
 # =======
